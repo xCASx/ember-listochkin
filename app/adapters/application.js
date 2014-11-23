13376985706d73e16a987e6a3eac6be7f90ce77c
Maxim Kolesnikov <MKolesnikov@luxoft.com>
2014-11-23 15:57:28 +0200

    Fix firebase

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 
-export default DS.RESTAdapter.extend({
+export default DS.FirebaseAdapter.extend({
   firebase: new Firebase(config.firebaseUrl)
 });
